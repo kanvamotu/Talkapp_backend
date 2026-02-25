eca6387ca68ac50c7ddaf7dc63ba3e948e592e4d
kanvamotu <pranjalsingh2609@gmail.com>
2026-02-26 02:07:37 +0530

    Fix CORS for Netlify frontend

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ app.use(
   cors({
     origin: ["https://talkky.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
